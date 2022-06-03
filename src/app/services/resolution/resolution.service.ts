47c10e808e0a7ab179f6e8a4fdf4c99af21c2757
Diman1988 <diman7988@gmail.com>
2022-06-03 22:49:33 +0200

    Service check resolution done

--- src/app/services/resolution/resolution.service.ts
+++ src/app/services/resolution/resolution.service.ts
@@ -1,23 +1,67 @@
 import { Injectable } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from "@angular/cdk/layout"
+import { distinctUntilChanged, Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResolutionService {
-  private isMobile: boolean;
+  private isMobile = new BehaviorSubject(false);
 
-  private isTablet: boolean;
+  private isTablet = new BehaviorSubject(false);
 
-  constructor() {
-    this.isMobile = (window.innerWidth > 320 && window.innerWidth <= 480) ? true  : false;
-    this.isTablet = (window.innerWidth > 481 && window.innerWidth < 768) ? true  : false;
+  private isLarge = new BehaviorSubject(false);
+
+  private resolution$: Observable<BreakpointState>;
+
+  constructor(private breakpointObserver: BreakpointObserver) {
+    this.resolution$ = this.breakpointObserver
+      .observe([
+        Breakpoints.HandsetLandscape,
+        Breakpoints.HandsetPortrait,
+        Breakpoints.TabletPortrait,
+        Breakpoints.TabletLandscape,
+        Breakpoints.Large
+      ])
+      .pipe(
+        distinctUntilChanged(),
+      );
+
+    this.resolution$
+      .subscribe(
+        observer => {
+          const isMobile = breakpointObserver.isMatched([Breakpoints.HandsetLandscape, Breakpoints.HandsetPortrait]);
+          const isTablet = breakpointObserver.isMatched([Breakpoints.TabletPortrait, Breakpoints.TabletLandscape]);
+
+          this.isMobile.next(false);
+          this.isTablet.next(false);
+
+          if (isMobile) {
+            this.isMobile.next(true);
+          } else if (isTablet) {
+            this.isTablet.next(true);
+          } else {
+            this.isLarge.next(true);
+          }
+        }
+      )
   }
 
-  public isMobileResolution(): boolean {
-    return this.isMobile;
+  ngOnInit() { }
+
+  public isMobileResolution(): Observable<boolean> {
+    return this.isMobile.asObservable();
   }
 
-  public isTabletResolution(): boolean {
-    return this.isTablet;
+  public isTabletResolution(): Observable<boolean> {
+    return this.isTablet.asObservable();
+  }
+
+  public isLargeResolution(): Observable<boolean> {
+    return this.isLarge.asObservable();
+  }
+
+  public getResolution() {
+    return this.resolution$;
   }
 }
