a2ca5462f9cd7910564faf4cb49c970d26255de0
Diman1988 <diman7988@gmail.com>
2022-06-20 13:33:59 +0200

    test errors hotfix

--- src/app/header/containers/header-container/header-container.spec.ts
+++ src/app/header/containers/header-container/header-container.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { HeaderContainerComponent } from './header-container';
 
@@ -8,6 +9,7 @@ describe('HeaderContainerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [MatMenuModule],
       declarations: [ HeaderContainerComponent ]
     })
     .compileComponents();
